9dec5f792a75fd83b7673b4eff714b1bce71d40b
coder <wang202217946@163.com>
2025-08-29 14:21:44 +0800

    nono

--- number.js
+++ number.js
@@ -1,6 +1,43 @@
 $(document).ready(function () {
+    // 开始对文本进行编号
     function startNumber() {
-        $("#outPutText").text($("#inputText").val());
+        let inputText = $("#inputText").val();
+        let textList = inputText.split("\n");
+        let serialNumber = 1;
+        let outputText = "";
+
+        textList.forEach(element => {
+            outputText += serialNumber++ + ". " + element + "\n";
+        });
+
+        $("#outPutText").val(outputText);
     }
-    $("#btn").click(startNumber);
+    /**
+     * 清空文本
+     */
+    function clear() {
+        $("#outPutText").val("");
+        $("#inputText").val("");
+    }
+    /**
+     * 复制文本
+     */
+    function copy() {
+        $("#outPutText").select();
+        document.execCommand('copy');
+    }
+    $("#btn_start").click(startNumber);
+    $("#btn_clear").click(clear);
+    $("#btn_copy").click(copy);
+
+
+    $("#optionSelect").change(function () {
+        let selectedValue = $(this).val();
+
+        // 隐藏所有输入框
+        $("#optionSelectInput").siblings().hide();
+        // 显示所选输入框
+        $("#" + selectedValue + "Input").show();
+
+    });
 })
