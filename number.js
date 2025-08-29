aaac5ce182c32fa07f1c3f03709dec65b3458952
coder <wang202217946@163.com>
2025-08-29 19:48:27 +0800

    美化页面

--- number.js
+++ number.js
@@ -1,17 +1,33 @@
 $(document).ready(function () {
-    // 开始对文本进行编号
+    $("#btn_start").click(startNumber);
+    $("#btn_clear").click(clear);
+    $("#btn_copy").click(copy);
+    /**
+     * 根据用户选项选择，执行对应的编号方法
+     */
     function startNumber() {
-        if ($("#optionSelect").val() == "symbol") {
-            numberAfterSymbol($("#symbolInput input").val());
+        if ($("#optionSelect").val() == "symbol") { // 指定符号后添加编号
+            numberAfterSymbol($("#symbol").val());
+        } else if ($("#optionSelect").val() == "position") { // 指定位置后添加编号
+            positionAfterNumber($("#position").val());
         }
-
-
-
     }
-    function numberAfterSymbol(symbol) {
-        console.log(symbol)
+
+    /**
+     * 将输入框中的文本转换成列表
+     * @returns 包含文本的列表
+     */
+    function inputTextToList() {
         let inputText = $("#inputText").val();
         let textList = inputText.split("\n");
+        return textList;
+    }
+    /**
+     * 指定符号后面添加编号
+     * @param {*} symbol 指定的符号
+     */
+    function numberAfterSymbol(symbol) {
+        let textList = inputTextToList();
         let serialNumber = 1;
         let outputText = "";
         textList.forEach(element => {
@@ -21,6 +37,28 @@ $(document).ready(function () {
                 outputText += element + "\n";
             }
         });
+        $("#outPutText").val(outputText);
+    }
+    /**
+     * 指定位置后面添加编号
+     * @param {*} position 指定的位置
+     */
+    function positionAfterNumber(position) {
+        let textList = inputTextToList();
+        let serialNumber = 1;
+        let outputText = "";
+        let pos = position;
+        textList.forEach(element => {
+            position = pos;
+            // 处理负数或者超出字符串长度
+            if (position < 0) {
+                position = 0;
+            } else if (position > element.length) {
+                position = element.length;
+            }
+            // 插入字符 
+            outputText += element.slice(0, position) + serialNumber++ + ". " + element.slice(position) + "\n";
+        })
         $("#outPutText").val(outputText);
     }
     /**
@@ -37,18 +75,14 @@ $(document).ready(function () {
         $("#outPutText").select();
         document.execCommand('copy');
     }
-    $("#btn_start").click(startNumber);
-    $("#btn_clear").click(clear);
-    $("#btn_copy").click(copy);
-
 
     $("#optionSelect").change(function () {
         let selectedValue = $(this).val();
 
         // 隐藏所有输入框
-        $("#optionSelectInput").siblings().hide();
+        $(".settings").children("input").hide();
         // 显示所选输入框
-        $("#" + selectedValue + "Input").show();
+        $("#" + selectedValue).show();
 
     });
 })
